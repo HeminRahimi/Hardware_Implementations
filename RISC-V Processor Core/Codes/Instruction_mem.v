e52fc2037afc92346ddf09e1b2be69714396c694
Hemin Rahimi <rahimihemn@gmail.com>
2026-02-08 20:30:44 +0330

    Update Instruction_mem.v

--- RISC-V Processor Core/Codes/Instruction_mem.v
+++ RISC-V Processor Core/Codes/Instruction_mem.v
@@ -18,42 +18,48 @@
 			Inst_Mem[2] = 8'h00;
 			Inst_Mem[3] = 8'h00;
 
-			// Instruction at 0: add x10, x1, x2
+			// Instruction at 4: add x10, x1, x2
 			Inst_Mem[4] = 8'h33;  // LSB
 			Inst_Mem[5] = 8'h85;
 			Inst_Mem[6] = 8'h20;
 			Inst_Mem[7] = 8'h00;  // MSB
 
-			// Instruction at 0: sub x30, x3, x2
+			// Instruction at 8: sub x30, x3, x2
 			Inst_Mem[8] = 8'hB3;
 			Inst_Mem[9] = 8'h0F;
 			Inst_Mem[10] = 8'h31;
 			Inst_Mem[11] = 8'h40;
 
-			// Instruction at 8: addi x30, x2, 15
+			// Instruction at 12: addi x30, x2, 15
 			Inst_Mem[12]  = 8'h03;
 			Inst_Mem[13]  = 8'h0F;
 			Inst_Mem[14] = 8'hF1;
 			Inst_Mem[15] = 8'h00;
 
-			// Instruction at 12: sw x20, 8(x5)
+			// Instruction at 16: sw x20, 8(x5)
                                            
 			Inst_Mem[16] = 8'h23;
 			Inst_Mem[17] = 8'h02;
 			Inst_Mem[18] = 8'h41;
 			Inst_Mem[19] = 8'h01;
 
-			// Instruction at 16: lw x8 0(x15)
+			// Instruction at 20: lw x8 0(x15)
 			Inst_Mem[20] = 8'h03;
 			Inst_Mem[21] = 8'h84;
 			Inst_Mem[22] = 8'h07;
 			Inst_Mem[23] = 8'h00;
 
-			// Instruction at 20: add x30 x8 x0 
+			// Instruction at 24: add x30 x8 x0 
 			Inst_Mem[24] = 8'h33;
 			Inst_Mem[25] = 8'h0F;
 			Inst_Mem[26] = 8'h04;
 			Inst_Mem[27] = 8'h00;
+			
+			// Instruction at 28:  beq x8 x8 4
+			Inst_Mem[28] = 8'h63;
+			Inst_Mem[29] = 8'h02;
+			Inst_Mem[30] = 8'h84;
+			Inst_Mem[31] = 8'h00;
 
 	    end
 			
